81719bbfd24171e2e0e089f7eedc606a74a68dba
devops2561 <devops2561@gmail.com>
2018-09-26 22:39:10 +0700

    Init commit

---
+++ src/main/java/com/tomyum/spring/TomyumApiApplication.java
@@ -0,0 +1,12 @@
+package com.tomyum.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomyumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomyumApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tomyum/spring/TomyumApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tomyum.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TomyumApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
